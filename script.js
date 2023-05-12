c7568bce545c855845f54412a7a01efa6a6eee4d
yanakin394 <yana.germatskaya@gmail.com>
2023-05-12 19:47:46 +0300

    js for options

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// пишу код, для появления доп.опции выбора модели, при выборе определенной марки автомобиля
-// прописываю константы для необходимых элементов
-const carType = document.getElementById('car_type');
+//константы необ.элементов
 const carModel = document.querySelector('.car__model');
+const totalPrice = document.querySelector('.total__price');
+
+//достаем модели автомобилей
 const reanault = document.getElementById('Reanault');
 const opel = document.getElementById('Opel');
 const ford = document.getElementById('Ford');
+
+//селектор
+const carType = document.getElementById('car_type');
+
+//радиокнопки
+const radioFuelType = document.querySelectorAll('input[name="fuel type"]');
+const radioCondition = document.querySelectorAll('input[name="condition"]');
+
 
 //обработчик событий для селектора
 carType.addEventListener('change', () => {
@@ -12,8 +21,8 @@ carType.addEventListener('change', () => {
         carModel.innerHTML = `
         <label for="car_model">Модель автомобиля
         <select name="Car Model" id="reanault_models">
-            <option value="Duster">Duster</option>
-            <option value="Logan">Logan</option>
+            <option value="2500">Duster</option>
+            <option value="2000">Logan</option>
             <option value="Kaptur">Kaptur</option>
             <option value="Sandero">Sandero</option>
         </select>
@@ -40,7 +49,7 @@ carType.addEventListener('change', () => {
     </label>`;
     } 
 });
-// 
+// код, для вывода опций кол-ва владельцев, при выборе опции Поддержанный
 const usedCarOptions = document.querySelector('.used__car-options');
 const usedCar = document.getElementById('used');
 usedCar.addEventListener('change', () => {
@@ -52,3 +61,16 @@ usedCar.addEventListener('change', () => {
         </label>`;
     }
 })
+//константы селекторов моделей
+const reanaultModels = document.getElementById('reanault_models'); 
+console.log(reanaultModels);
+//не работает
+const radioUsed = document.querySelectorAll('input[name="owners"]');
+console.log(radioUsed);
+
+// логика калькулятора
+const calcButton = document.getElementById('calc__button');
+/* calcButton.addEventListener('click', => {
+    if()
+    console.log();
+}) */
